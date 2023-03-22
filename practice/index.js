48379eaeb712c157cb1a5eaffbae5f078f755a99
Tushar <tusharbehera32@gmail.com>
2023-03-23 04:36:19 +0530

    import values from another file

---
+++ practice/index.js
@@ -0,0 +1,5 @@
+// import value from another file "app.js"
+const app = require('./app')
+console.log(app)  //use to access the whole object
+console.log(app.c()) //use to call particular value from the object
+

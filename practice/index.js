9dc258e0384372a58d1d6bdc602adf16b8e0220b
Tushar <tusharbehera32@gmail.com>
2023-03-23 04:57:44 +0530

    use of filter() function

--- practice/index.js
+++ practice/index.js
@@ -3,3 +3,21 @@ const app = require('./app')
 console.log(app)  //use to access the whole object
 console.log(app.c()) //use to call particular value from the object
 
+
+//Using of filter() function to filter array in diff ways
+
+const arr =[2,4,5,7,1,3,8,3]    //array created
+
+arr.filter((item)=>{    //help to traverse accross the array
+    console.log(item)
+})
+
+let result = arr.filter((value)=>{  //help to check repetation of particular value inside arr
+    return value === 3              
+})
+ console.log(result)
+
+ let result_1 = arr.filter((value)=>{  //help to check value greater or less then the input value 
+    return value > 3              
+})
+ console.log(result_1)

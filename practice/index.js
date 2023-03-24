37264659b672110c9d9c9ab608382a7364308ca9
Tushar <tusharbehera32@gmail.com>
2023-03-24 15:56:24 +0530

    node module file details updated

--- practice/index.js
+++ practice/index.js
@@ -1,3 +1,5 @@
 //creating package.json use to store all detail and modules use in project
 //npm init > to initialize the file
 // npm is use tio install the modules known as "node package manager"
+//lets install a module it will create node module file and store all module details  <<-- IMP
+//in package.json dependencies section that installed modules details are nw automatically added

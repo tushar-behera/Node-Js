ae83caa19a6e4f48077e515ad67dc4241f6e8323
Tushar <tusharbehera32@gmail.com>
2023-03-28 10:21:03 +0530

    Practice a external module 'Chalk'

--- practice/index.js
+++ practice/index.js
@@ -1,8 +1,36 @@
-//creating package.json use to store all detail and modules use in project
-//npm init > to initialize the file
-// npm is use tio install the modules known as "node package manager"
-//lets install a module it will create node module file and store all module details  <<-- IMP
-//in package.json dependencies section that installed modules details are nw automatically added
+//Task using " Chalk Module "
+//using chalk less then V 5.0 so that it will work
 
-var colors = require('colors'); //importing the module here
-console.log('inverse the color'.rainbow); //use to print as the module work
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
+
+//or
+
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
+
+// Easily define your own themes :==>
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+log(error('Error!'));
+log(warning('Warning!'));

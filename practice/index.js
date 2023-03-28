4fbe1dcb1a4af77e0cc9c3a5058dd0185da97d20
Tushar <tusharbehera32@gmail.com>
2023-03-28 09:47:20 +0530

    Use of external modules updated

--- practice/index.js
+++ practice/index.js
@@ -3,3 +3,6 @@
 // npm is use tio install the modules known as "node package manager"
 //lets install a module it will create node module file and store all module details  <<-- IMP
 //in package.json dependencies section that installed modules details are nw automatically added
+
+var colors = require('colors'); //importing the module here
+console.log('inverse the color'.rainbow); //use to print as the module work

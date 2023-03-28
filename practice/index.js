22d0d60d962f2fa378f24171375a5985f63069f5
Tushar <tusharbehera32@gmail.com>
2023-03-28 10:41:51 +0530

    adding .gitignore file and learn use of ' npm install '

--- practice/index.js
+++ practice/index.js
@@ -1,36 +1,5 @@
-//Task using " Chalk Module "
-//using chalk less then V 5.0 so that it will work
-
-const chalk = require('chalk');
-console.log(chalk.blue('Hello world!'));
-
-//or
-
-const log = console.log;
-
-// Combine styled and normal strings
-log(chalk.blue('Hello') + ' World' + chalk.red('!'));
-
-// Compose multiple styles using the chainable API
-log(chalk.blue.bgRed.bold('Hello world!'));
-
-// Pass in multiple arguments
-log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
-
-// Nest styles
-log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
-
-// Nest styles of the same type even (color, underline, background)
-log(chalk.green(
-	'I am a green line ' +
-	chalk.blue.underline.bold('with a blue substring') +
-	' that becomes green again!'
-));
-
-// Easily define your own themes :==>
-
-const error = chalk.bold.red;
-const warning = chalk.keyword('orange');
-
-log(error('Error!'));
-log(warning('Warning!'));
+// Use of 'npm install' :==>
+// node modules are large files so no need to push it to github or send it to any one else
+//but it is necessary to attach the 'package.jshon' file to install node modules other wise it cant be fetch what are the modules we use in our project
+//now run : npm install  ==> it will bring the node module files to the project.
+//attach the .gitignore file to the project and inside it put the node modules file so it will ignore the push command.

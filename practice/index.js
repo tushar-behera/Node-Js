8df72eafdea7b073e1a6ad737a14b62be9305a27
Tushar <tusharbehera32@gmail.com>
2023-03-28 11:09:48 +0530

    update about 'nodemon' package

--- practice/index.js
+++ practice/index.js
@@ -1,5 +1,9 @@
-// Use of 'npm install' :==>
-// node modules are large files so no need to push it to github or send it to any one else
-//but it is necessary to attach the 'package.jshon' file to install node modules other wise it cant be fetch what are the modules we use in our project
-//now run : npm install  ==> it will bring the node module files to the project.
-//attach the .gitignore file to the project and inside it put the node modules file so it will ignore the push command.
+//Use of "Nodemon"
+//nodemon run in background untill you stop it with : CTRL + C
+//as it run continously while modify any API or Server or any program we don't have to run node again and agin 
+//it will show the output each time the file save 
+//command to install nodemon : npm install -g nodemon
+// -g : stands for global instalation so that it will run in any project directory all over the computer not on a particular directory
+//to use nodemon use : nodemon file_name 
+
+
